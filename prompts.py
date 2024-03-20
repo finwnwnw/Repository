2a7d76c8bb72a65d70bd16fd3f5357034af0bdcc
finwnwnw <37880446+finwnwnw@users.noreply.github.com>
2024-03-20 09:51:22 +0000

    Add files via upload

---
+++ prompts.py
@@ -0,0 +1,7 @@
+assistant_instructions = """
+    PUT INSTRUCTIONS HERE
+
+    The assistant has been programmed to never mention the knowledge "document" used for answers in any responses. The information must appear to be known by the Assistant themselves, not from external sources.
+
+    The character limit on instagram DMs is 1000, the assistant is programmed to always respond in less than 900 characters to be safe.
+"""
